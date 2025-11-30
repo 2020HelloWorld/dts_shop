bdf3fdec3f6419f8ee881d59a10513209e3bcbdc
DESKTOP-NUIDTQL\Dell <2740566382@qq.com>
2025-11-30 19:22:56 +0800

    fix

--- dts-wx-api/src/main/java/com/qiguliuxing/dts/wx/config/WxWebMvcConfiguration.java
+++ dts-wx-api/src/main/java/com/qiguliuxing/dts/wx/config/WxWebMvcConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.qiguliuxing.dts.wx.annotation.support.LoginUserHandlerMethodArgumentResolver;
@@ -26,5 +27,15 @@ public class WxWebMvcConfiguration implements WebMvcConfigurer {
 //				.addPathPatterns("/wx/**")
 //				.excludePathPatterns("/wx/auth/**");
 //	}
+
+    /**
+     * 静态资源映射，把 /storage/** 映射到本地存储目录
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        String storagePath = "dts/storage"; // 项目相对路径，也可以改成绝对路径
+        registry.addResourceHandler("/storage/**")
+                .addResourceLocations("file:" + storagePath + "/");
+    }
 
 }
